7d8791f939003a367fe77ba324d521fbe8be1cf4
Johannes Weber <johannes@14oranges.com>
2023-05-10 18:49:53 -0700

    add dotenv

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT ?? 3000;
 
 // Load the router, which in turns instantiates the Device Pool.
 const kasaRouter = require('./routers/kasaRouter');
