814d7cb31e4ea44e39852fbca086e5c873f10a77
Johannes Weber <johannes@14oranges.com>
2023-06-10 09:39:34 -0700

    implement getDaytimePercent and related functions

--- ll-kasa-bridge/helpers/jDateTimeUtils.js
+++ ll-kasa-bridge/helpers/jDateTimeUtils.js
@@ -1,27 +1,125 @@
-import { getSunrise, getSunset } from 'sunrise-sunset-js';
+import sunriseSunsetJs from 'sunrise-sunset-js';
 
-const lat = 49.2827;
-const long = -123.1207;
+// Vancouver
+const DEFAULT_LAT = 49.28;
+const DEFAULT_LONG = -123.12;
 
+const DEFAULT_DAWN_DUSK_LENGTH = 60 * 60 * 1000;
 
-Date.prototype.stdTimezoneOffset = function () {
-  var jan = new Date(this.getFullYear(), 0, 1);
-  var jul = new Date(this.getFullYear(), 6, 1);
-  return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+// Internal use: get lat/long when none are provided
+const _getCoords = (coords) => {
+  if (coords) {
+    return coords;
+  }
+
+  return {
+    lat: DEFAULT_LAT,
+    long: DEFAULT_LONG,
+  }
 }
 
-Date.prototype.isDstObserved = function () {
-  return this.getTimezoneOffset() < this.stdTimezoneOffset();
-}
-
-const isDaytime = () => {
+// Get today's sunset from library
+const getSunset = (coords) => {
+  const { lat, long } = _getCoords(coords);
 
   const now = new Date();
-  const hours = now.getHours();
+  // Need to pass this into getSunset, or it gets yesterday's sunset time (?)
+  const plus1Day = new Date(now.getTime() + 24 * 60 * 60 * 1000);  
 
-  return (hours > 21 && hours < 7);  
+  return sunriseSunsetJs.getSunset(lat, long, plus1Day);
 }
 
+// Get today's sunrise from library
+const getSunrise = (coords) => {
+  const { lat, long } = _getCoords(coords);
+  return sunriseSunsetJs.getSunrise(lat, long);
+}
+
+/**
+ * Are we between today's sunrise and sunset?
+ * @returns bool
+ */
+const isDaytime = (date = null, coords = null) => {
+  const { lat, long } = _getCoords(coords);
+
+  const now = date ? date : new Date();
+  const sunset = getSunset({lat, long});
+  const sunrise = getSunrise({lat, long});
+
+  return sunrise < now && sunset > now;  
+}
+
+/**
+ * Is it dark out (are we before sunrise or after sunset)?
+ * @returns bool
+ */
+ const isNighttime = (date = null, coords = null) => !isDaytime(date, coords);
+ 
+
+ /**
+  * Return a percentage with indicates fully day (1), fully night (0), or dusk/dawn (0 < result < 1)
+  * 
+  * @param {*} transitionTime 
+  * @param {*} date 
+  * @param {*} latitude 
+  * @param {*} longitude 
+  */
+const getDaytimePercent = (transitionTime = null, date = null, coords = null) => {
+  const { lat, long } = _getCoords(coords);
+
+  if (!transitionTime) {
+    transitionTime = DEFAULT_DAWN_DUSK_LENGTH;
+  }
+
+  if (!date) {
+    date = new Date();
+  }
+
+  // Get start and end of transition period, with the center beeing the current time (or date passed in)
+  const transitionStart = new Date();
+  transitionStart.setTime(date.getTime() - (transitionTime / 2));
+
+  const transitionEnd = new Date();
+  transitionEnd.setTime(transitionStart.getTime() + transitionTime);
+
+  if (isDaytime(date, coords) && isDaytime(transitionStart, coords) && isDaytime(transitionEnd, coords)) {    
+    // Fully day: post dawn, pre dusk    
+    return 1;
+    
+  } else if (isNighttime(date, coords) && isNighttime(transitionStart, coords) && isNighttime(transitionEnd, coords)) {
+    // Fully night: post dusk, pre dawn    
+    return 0;
+
+  } else {
+    // Twilight
+    const isDawn = isDaytime(transitionEnd);
+
+    const twilightStart = new Date();
+    twilightStart.setTime(
+      (isDawn ? getSunrise(coords).getTime() : getSunset(coords).getTime())
+      - (transitionTime / 2)
+    );
+
+    const twilightEnd = new Date();
+    twilightEnd.setTime(twilightStart.getTime() + transitionTime);
+
+    const transitionPercent = (date.getTime() - twilightStart.getTime()) / transitionTime;
+
+    return isDawn ? transitionPercent : 1 - transitionPercent;
+  }
+}
+
+const getNighttimePercent = (transitionTime = null, date = null, coords = null) => 1 - getDaytimePercent(transitionTime, date, coords);
+
+const isFullyDaytime = (transitionTime = null, date = null) => getDaytimePercent(transitionTime, date) === 1;
+
+
 export {
-  isDaytime,
+  getSunrise,
+  getSunset,
+  getDaytimePercent,
+  getNighttimePercent,
+  isDaytime,  
+  isFullyDaytime,
+  isNighttime,
 }
