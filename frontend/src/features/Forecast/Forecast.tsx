b3c3220558a0cb515e35515593e7a670bdfe74c8
Johannes Weber <johannes@drweber.de>
2023-11-27 17:59:41 -0800

    updates to the short term forecast, now displaying four 3-hour blocks

--- frontend/src/features/Forecast/Forecast.tsx
+++ frontend/src/features/Forecast/Forecast.tsx
@@ -30,24 +30,37 @@ function Forecast() {
 
     const { state } = weatherService;
 
-    const displayData = getDisplayData(state, 0);
+    let displayData = getDisplayData(state, 0);
 
-    const styleTemp = {
-        color: tempToColor(displayData.tempC),
-    };
+    const allDisplayData = getAllDisplayData(state, 4);
 
-    const styleTrend = {
-        color: displayData.trendColor,
-    };
+    const displayDataJsx = allDisplayData.map((item, index) => {
+
+      const styleTemp = {
+          color: tempToColor(displayData.tempC),
+      };
+
+      return (
+          <div key={index} className="forecast-item-container">
+              <div className="forecast-text-container">
+                  <div className="forecast-text-time">{item.dateText}</div>
+                  <div className="forecast-text-temp" style={styleTemp}>
+                      {item.tempC}
+                  </div>
+              </div>
+              <div className="forecast-icon-container">
+                  <img className="forecast-icon" src={item.iconUrl} />
+              </div>
+          </div>
+      );
+    })
+    
 
     return (
         <div className="fullscreen-panel-temperature">
-            <div className="thermometer-label">3-hour Forecast</div>
-            <div className="thermometer-temp" style={styleTemp}>
-                {displayData.tempC ?? 'N/A'}
-            </div>
-            <div className="thermometer-trend" style={styleTrend}>
-                {displayData.weatherMain ?? 'N/A'}
+            <div className="forecast-label">Shortterm Forecast</div>
+            <div className="forecast-display-data-container">
+                {displayDataJsx}
             </div>
         </div>
     );
@@ -56,16 +69,41 @@ function Forecast() {
 
 function getDisplayData(state, n) {
     const extractedData: any = {}
-
-    if  (extractedData.weatherMain = state.list && state.list[n] && state.list[n].weather && state.list[n].weather.length) {
+    
+    if  (extractedData.weatherMain = state.list && state.list[n] && state.list[n].weather && state.list[n].weather.length) {      
+      extractedData.timestamp = state.list[n].dt;
+      extractedData.dateText = formatForecastDate(extractedData.timestamp);
+      extractedData.dateHour = new Date(extractedData.timestamp * 1000).getHours();
       extractedData.cityName = state.city?.name;
-      extractedData.weatherMain = state.list[n].weather[0].main;
-      extractedData.tempC = (state.list[n].main.temp - 273).toFixed() + "°C"; // Comes in Kelvin
+      extractedData.weatherMain = state.list[n].weather[0].description;
+      extractedData.icon = state.list[n].weather[0].icon;
+      extractedData.iconUrl = `https://openweathermap.org/img/w/${extractedData.icon}.png`;
+      extractedData.tempC = (state.list[n].main.temp - 273).toFixed() + "°"; // Comes in Kelvin
     }
 
-    
-
     return extractedData;
+}
+
+function getAllDisplayData(state, maxLength: number = 10, skipNight = true) {
+  if (!(state && state.list)) {
+    return [];
+  }
+
+  const displayData: any[] = [];
+
+  state.list.forEach((item, index) => {
+    const itemsCollected = displayData.length;
+
+    if (itemsCollected < maxLength) {
+        const displayDataItem = getDisplayData(state, index);
+        if (displayDataItem.dateHour > 6 && displayDataItem.dateHour < 22) {
+            displayData.push(displayDataItem);
+        }        
+    }
+    
+  })
+  
+  return displayData;
 }
 
 function tempToColor(tempC) {
@@ -89,6 +127,21 @@ function tempToColor(tempC) {
     }
 
     return `#${color}`;
+}
+
+function formatForecastDate(unixTimestamp) {
+    // Create a new Date object using the timestamp (multiply by 1000 to convert seconds to milliseconds)
+    const date = new Date(unixTimestamp * 1000);
+
+    // Get the hour and minute
+    const hour = date.getHours();
+
+    // Format the result
+    const formattedDate = `${
+        hour % 12 === 0 ? 12 : hour % 12
+    }${hour >= 12 ? "pm" : "am"}`;
+
+    return formattedDate;
 }
 
 export default Forecast;
