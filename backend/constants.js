7994e903d22c710d68d564f9ef81810517bab7cd
Johannes Weber <johannes@drweber.de>
2024-09-16 20:28:23 -0700

    Backend: had missed to commit constants.js for the lat/lon update

--- backend/constants.js
+++ backend/constants.js
@@ -6,74 +6,74 @@ const DAY = 24 * HOUR;
 const WEEK = 7 * DAY;
 
 const constants = {
+    DEBUG: {
+        debug: false,
+        channels: [40],
+    },
 
-  DEBUG: {
-    debug: false,
-    channels: [40],
-  },
+    // Time
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY,
+    WEEK,
 
-  // Time
-  SECOND, 
-  MINUTE, 
-  HOUR,
-  DAY,
-  WEEK,
+    // 3048 Venables St
+    //DEFAULT_LAT: 49.27631,
+    //DEFAULT_LON: -123.0402,
 
-  // 3048 Venables St
-  DEFAULT_LAT: 49.27631,
-  DEFAULT_LON: -123.04020,
+    // 5 Williston Crescent
+    DEFAULT_LAT: 55.3349,
+    DEFAULT_LON: -123.0842,
 
-  // When a device goes offline the saved powerstate for its wrapper will be discarded after this timeout.
-  DEVICE_POWERSTATE_TIMEOUT: 30 * SECOND,
+    // When a device goes offline the saved powerstate for its wrapper will be discarded after this timeout.
+    DEVICE_POWERSTATE_TIMEOUT: 30 * SECOND,
 
+    // Device types
+    DEVICETYPE_KASA_SMARTPLUGSWITCH: "IOT.SMARTPLUGSWITCH",
+    DEVICETYPE_KASA_SMARTBULB: "IOT.SMARTBULB",
 
+    DEVICETYPE_ESP_THERMOMETER: "ESP Thermometer",
+    DEVICETYPE_ESP_RELAY: "ESP_RELAY",
+    DEVICETYPE_ESP_MAILBOX: "ESP_MAILBOX",
 
-  // Device types
-  DEVICETYPE_KASA_SMARTPLUGSWITCH: 'IOT.SMARTPLUGSWITCH',
-  DEVICETYPE_KASA_SMARTBULB: 'IOT.SMARTBULB',
+    DEVICETYPE_VIRTUAL: "Virtual Device",
 
-  DEVICETYPE_ESP_THERMOMETER: 'ESP Thermometer',
-  DEVICETYPE_ESP_RELAY: 'ESP_RELAY',
-  DEVICETYPE_ESP_MAILBOX: 'ESP_MAILBOX',
+    // Subtypes
+    SUBTYPE_BULB: "bulb",
+    SUBTYPE_LED_STRIP: "led-strip",
+    SUBTYPE_PLUG: "plug",
+    SUBTYPE_SWITCH: "switch",
 
-  DEVICETYPE_VIRTUAL: 'Virtual Device',
+    SUBTYPE_THERMOMETER: "thermometer",
+    SUBTYPE_MAIL_COMPARTMENT: "mailbox",
 
-  // Subtypes
-  SUBTYPE_BULB: 'bulb',
-  SUBTYPE_LED_STRIP: 'led-strip',
-  SUBTYPE_PLUG: 'plug',
-  SUBTYPE_SWITCH: 'switch',  
+    SUBTYPE_AIR_FAN: "fan",
+    SUBTYPE_AIR_HEAT: "heater",
+    SUBTYPE_AIR_AC: "a/c",
+    SUBTYPE_CHRISTMAS_TREE: "christmas-tree",
+    SUBTYPE_LIGHT: "light", // A non-smart plug-in-light
+    SUBTYPE_ENTERTAINMENT: "entertainment",
 
-  SUBTYPE_THERMOMETER: 'thermometer',
-  SUBTYPE_MAIL_COMPARTMENT: 'mailbox',
+    SUBTYPE_THERMOSTAT: "thermostat",
+    SUBTYPE_TIMER: "timer",
+    SUBTYPE_WEATHER_SERVICE: "weatherService",
+    SUBTYPE_DYNFORMS_SERVICE: "dynformsService",
+    SUBTYPE_DAV_SERVICE: "davService",
+    SUBTYPE_CLOCK: "clock",
+    SUBTYPE_MASTER_SWITCH: "masterSwitch",
 
-  SUBTYPE_AIR_FAN: 'fan',
-  SUBTYPE_AIR_HEAT: 'heater',
-  SUBTYPE_AIR_AC: 'a/c',
-  SUBTYPE_CHRISTMAS_TREE: 'christmas-tree',
-  SUBTYPE_LIGHT: 'light', // A non-smart plug-in-light
-  SUBTYPE_ENTERTAINMENT: 'entertainment',  
+    // Service names
+    SERVICE_PERIODIC_FILTER: "Periodic Filter Service",
+    SERVICE_BACKEND: "backend",
+    SERVICE_BACKEND_FLIP: "backend (flip)",
+    SERVICE_COMMAND_CACHE: "Command Cache Service",
 
-  SUBTYPE_THERMOSTAT: 'thermostat',
-  SUBTYPE_TIMER: 'timer',
-  SUBTYPE_WEATHER_SERVICE: 'weatherService',
-  SUBTYPE_DYNFORMS_SERVICE: 'dynformsService',
-  SUBTYPE_DAV_SERVICE: 'davService',
-  SUBTYPE_CLOCK: 'clock',
-  SUBTYPE_MASTER_SWITCH: 'masterSwitch',
+    // Default Location
+    DEVICE_DEFAULT_LOCATION_ID: "loc-default",
 
-  // Service names
-  SERVICE_PERIODIC_FILTER: 'Periodic Filter Service',
-  SERVICE_BACKEND: 'backend',
-  SERVICE_BACKEND_FLIP: 'backend (flip)',
-  SERVICE_COMMAND_CACHE: 'Command Cache Service',
-
-  // Default Location
-  DEVICE_DEFAULT_LOCATION_ID: 'loc-default',
-
-  // Default Poll interval
-  ESP_DEFAULT_POLL_INTERVAL: 10 * SECOND,
-  
+    // Default Poll interval
+    ESP_DEFAULT_POLL_INTERVAL: 10 * SECOND,
 };
 
 constants.SUBTYPES = [
